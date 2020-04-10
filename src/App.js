50e4fcd0a9c861f5dc25fe1c79e61e9b7b472f97
FichoZee <filip.zafran@gmail.com>
2020-04-10 20:42:32 +0200

    added table, doesnt render

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Table from "./components/Grid";
 
 export default class App extends Component {
   render() {
     return (
       <React.Fragment>
         <Header></Header>
-
-        <div></div>
+        <Table></Table>
       </React.Fragment>
     );
   }
--- src/components/Grid.js
+++ src/components/Grid.js
@@ -1,6 +1,39 @@
 import React from "react";
 
-function Grid() {
-  return <div className="Grid"></div>;
+function Table() {
+  return (
+    <table className="table">
+      <tbody>
+        <tr>
+          <th>Player</th>
+          <th>1</th>
+          <th>2</th>
+          <th>3</th>
+          <th>4</th>
+          <th>5</th>
+          <th>6</th>
+          <th>7</th>
+          <th>8</th>
+          <th>9</th>
+          <th>10</th>
+          <th>TOT</th>
+        </tr>
+        <tr>
+          <td>Player 1</td>
+          <td></td>
+          <td> </td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tbody>
+    </table>
+  );
 }
-export default Grid;
+
+export default Table;
